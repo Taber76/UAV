50855e52f70ecade9fd206759d14fe5bbc0120e6
Tabare Bermudez <tabarebermudez@gmail.com>
2023-08-03 15:40:35 -0300

    [FEATURE] NestJS Server

--- serverRaspberry/server/controllers/pixhawkController.py
+++ serverRaspberry/server/controllers/pixhawkController.py
@@ -22,35 +22,3 @@ def post_uav_msg_controller(request_data):
     command = Pixhawk.get_message(
         request_data['message'], param1, param2, param3, param4, param5, param6, param7)
     return command
-
-
-# Arm UAV -> Arm (arm = 1) / Disarm (arm = 0)
-def arm_uav_controller(arm):
-    # verificacion valor de arm
-    try:
-        response = Pixhawk.arm(arm)
-        return response
-    except:
-        print("--> Error arm/disarm UAV")
-        pass
-
-
-def takeoff_uav_controller(launch_angle, latitude, longitude, altitude):
-    # verificacion valor de variables
-    try:
-        response = Pixhawk.takeoff(launch_angle, latitude, longitude, altitude)
-        return response
-    except:
-        print("--> Error takeoff UAV")
-        pass
-
-
-# Go to waypoint
-def go_to_waypoint_controller(lat, lon, alt):
-    # verificacion valor de variables
-    try:
-        response = Pixhawk.go_to_waypoint(lat, lon, alt)
-        return response
-    except:
-        print("--> Error go to waypoint")
-        pass
